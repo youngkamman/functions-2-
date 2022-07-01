103b9e6a18d6c21c6075cd5e778c74613f2c1d78
Kamerron Pace <youngkamman@gmail.com>
2022-06-30 21:28:10 -0400

    second snap

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,8 +18,12 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(function(elem) {
+  return elem % 2 === 0
+})
 
+// // with an arrow function
+// const evenNumbers = mixedNumbers.filter(num => num % 2 === 0)
 
 
 ////////// PROBLEM 2 //////////
@@ -39,7 +43,13 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+// CODE HERE
+const postTaxPrices = prices.map(function(elem) {
+  return elem * 1.07
+}) 
+
+// // with an arrow function
+// const postTaxPrices = prices.map(num => num * 1.07)
 
 
 
@@ -57,8 +67,13 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+// CODE HERE
+const totalPopulation = populations.reduce(function(acc, elem) {
+  return acc + elem
+})
 
+// // with an arrow function
+// const totalPopulation = populations.reduce((a, c) => a + c)
 
 
 ////////// PROBLEM 4 //////////
@@ -82,7 +97,14 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter(function(elem) {
+  // could have used bracket notation
+  // return elem["CP"] > 200
+  return elem.CP > 200
+})
+
+// // with an arrow function
+// const myStrongest = monstersInYourPocket.filter(monster => monster.CP > 200)
 
 
 
@@ -100,6 +122,14 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 // CODE HERE
+const orderTotals = orders.map(function(elem) {
+  // could have used bracket notation
+  // return elem["price"] + (elem["price"] * elem["tax"])
+  return elem.price + (elem.price * elem.tax)
+})
+
+// // with an arrow function
+// const orderTotals = orders.map(order => order.price + (order.price * order.tax))
 
 
 
@@ -120,3 +150,13 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+const bobsTotal = purchases.filter(function(elem) {
+  return elem.owner === "Bob"
+}).reduce(function(acc,elem) {
+  return acc + elem.price
+}, 0)
+
+// // with an arrow function 
+// const bobsTotal = purchases
+//   .filter(purchase => purchase.owner === "Bob")
+//   .reduce((a, c) => a + c.price, 0)
